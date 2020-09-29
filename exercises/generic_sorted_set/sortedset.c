f12a8f57587c11211215897f693e61a23fc91204
iarigby <marvinzem@gmail.com>
2020-09-29 11:23:44 +0400

    sorted set starter

---
+++ exercises/generic_sorted_set/sortedset.c
@@ -0,0 +1,14 @@
+
+void SetNew(sortedset *set, int elemSize,
+	int (*cmpfn)(const void *, const void *)) {
+
+}
+
+bool SetAdd(sortedset *set, const void *elemPtr) {
+
+}
+
+
+void *SetSearch(sorted *set, const void *elemPtr) {
+	
+}
