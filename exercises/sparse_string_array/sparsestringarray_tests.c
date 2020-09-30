151d83f4d00b70992ed7b55215aad0d629285ad9
gkhelo <gkhel18@freeuni.edu.ge>
2020-09-30 23:46:39 +0400

    added test cases

--- exercises/sparse_string_array/sparsestringarray_tests.c
+++ exercises/sparse_string_array/sparsestringarray_tests.c
@@ -36,6 +36,11 @@ void increaseCounter(int index, const char *str, void *auxData) {
 	(*ptr)++;
 }
 
+void increaseSymbolsCounter(int index, const char * str, void *auxData) {
+	int * ptr = auxData;
+	(*ptr) += strlen(str);
+}
+
 // Tests 
 
 bool Create_Dispose() {
@@ -71,6 +76,18 @@ bool Insert_Different_Indexes() {
 
 	SSADispose(&ssa);
 	return true;
+}
+
+bool Insert_Elems() {
+	sparsestringarray ssa;
+	SSANew(&ssa, 100, 25);
+
+	for (int i = 0; i < 100; i++) {
+		ASSERT(SSAInsert(&ssa, i, "elem") == true);
+	}
+
+	SSADispose(&ssa);
+	return true;	
 }
 
 bool Map_One_Elem() {
@@ -130,6 +147,46 @@ bool Replace_Elem() {
 	return true;
 }
 
+bool Calculate_Num_Chars() {
+	sparsestringarray ssa;
+	SSANew(&ssa, 200, 50);
+
+	int numChars = 15;
+	SSAInsert(&ssa, 21, "one");
+	SSAInsert(&ssa, 50, "two");
+	SSAInsert(&ssa, 102, "three");
+	SSAInsert(&ssa, 188, "four");
+
+	int counter = 0;
+	SSAMap(&ssa, increaseSymbolsCounter, &counter);
+
+	ASSERT(counter == numChars);
+
+	SSADispose(&ssa);
+	return true;
+}
+
+bool Calculate_Num_Elems2() {
+	sparsestringarray ssa;
+	SSANew(&ssa, 200, 50);
+
+	int numElems = 3;
+	SSAInsert(&ssa, 21, "one");
+	SSAInsert(&ssa, 50, "two");
+	SSAInsert(&ssa, 21, "three");
+	SSAInsert(&ssa, 21, "four");
+	SSAInsert(&ssa, 50, "five");
+	SSAInsert(&ssa, 102, "six");
+
+	int counter = 0;
+	SSAMap(&ssa, increaseCounter, &counter);
+
+	ASSERT(counter == numElems);
+
+	SSADispose(&ssa);
+	return true;
+}
+
 void Test(char* name, bool(*test_fn)()) {
   printf("++ RUNNING TEST: %s\n", name);
   if (test_fn()) {
@@ -144,9 +201,12 @@ int main() {
 	Test("Creates SSA", Create_Dispose);
 	Test("Inserts two strings at the same index", Insert_Same_Index);
 	Test("Inserts several strings at the different indexes", Insert_Different_Indexes);
+	Test("Inserts strings at every position", Insert_Elems);
 	Test("Adds one string to SSA and maps over it", Map_One_Elem);
 	Test("Calculates number of strings in SSA with SSAMap function", Calculate_Num_Elems);
 	Test("Adds three strings at the same index and checks if last string is saved", Replace_Elem);
-	
+	Test("Calculates overall number of symbols in SSA with SSAMap function", Calculate_Num_Chars);
+	Test("Inserts strings at the same indexes and calculates number of elems in SSA", Calculate_Num_Elems2);
+
 	return 0;
 }
